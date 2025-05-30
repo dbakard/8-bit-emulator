54d3e6b07841f8a389a9a54646e061df024953d6
thall49 <thall49@vols.utk.edu>
2025-05-30 16:32:00 -0400

    Adding SDL2 testing

--- include/chip8.h
+++ include/chip8.h
@@ -1,1 +1,20 @@
+#pragma once
 
+class chip8 {
+    private:
+        unsigned char memory[4096];
+        unsigned char V[8];
+        unsigned short I;
+        unsigned short pc;
+        unsigned short opcode;
+        unsigned char gfx[64 * 32];
+        unsigned char delay_timer;
+        unsigned char sound_timer;
+        unsigned short stack[16];
+        unsigned short sp;
+        unsigned char key[16];
+
+    public:
+        void initialize();
+        void emulate_cycle();
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,27 @@
+#include <SDL2/SDL.h>
 #include <iostream>
 
 int main() {
-	std::cout << "Hello World!\n";
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("SDL2 Test",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        640, 480, SDL_WINDOW_SHOWN);
+
+    if (!window) {
+        std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
+        SDL_Quit();
+        return 1;
+    }
+
+    SDL_Delay(2000); // Display window for 2 seconds
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    std::cout << "SDL2 window shown successfully.\n";
+    return 0;
 }
+
